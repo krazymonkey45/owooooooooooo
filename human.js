2db0fcfb20be9426d4abab5f2fa8522bc261b474
Isaiah <isaiah.adams7895@gmail.com>
2019-06-24 14:29:01 +0430

    working on the goodies

---
+++ human.js
@@ -0,0 +1,20 @@
+function display(){
+    console.log()
+}
+//define functions
+class GODS{
+    constructor(name,age,height){
+        let.name = name;
+        let.age = age;
+        let.height= height;
+    }
+}
+//define parents
+let Isaiah = new GODS("Isaiah", 17, "5'8")
+//child class
+class Hometown extends GODS{
+
+}
+//instantiate yourself from the child
+display(roger.powerMove())
+display(roger.chess())
